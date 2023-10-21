1c4ecf2e9b5ef147219b11eb8d03b9ce92a79724
narramuni <nmspavan@gmail.com>
2023-10-21 12:38:17 +0530

    Add project files.

---
+++ SortingAndSearchingData/Program.cs
@@ -0,0 +1,47 @@
+﻿using StudentsDataLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAndSearchingData
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentDatabase = new StudentsDataBase();
+
+            while (true)
+            {
+                Console.WriteLine("1. Display all students");
+                Console.WriteLine("2. Search for a student by name");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        studentDatabase.DisplayStudents();
+                        break;
+                    case 2:
+                        Console.Write("Enter the name of the student you want to search for: ");
+                        string searchName = Console.ReadLine();
+                        studentDatabase.SearchStudentByName(searchName);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+
+    
+    }
+}
---
+++ StudentsDataLibrary/StudentsDataBase.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsDataLibrary
+{
+    public class StudentsDataBase
+    {
+        private List<Student> students;
+
+        public StudentsDataBase()
+        {
+            students = new List<Student>();
+            ReadStudentDataFromFile(@"C:\Users\SAI PAVAN KUMAR\source\repos\SortingAndSearchingData\SortingAndSearchingStudentsData.txt"); 
+        }
+
+        public void ReadStudentDataFromFile(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                foreach (var line in File.ReadLines(fileName))
+                {
+                    var parts = line.Split(',');
+                    if (parts.Length == 5)
+                    {
+                        var student = new Student
+                        {
+                            Id = int.Parse(parts[0]),
+                            Name = parts[1],                            
+                            Address = parts[2],
+                            Class = parts[3],
+                            Grade = int.Parse(parts[4])
+                        };
+                        students.Add(student);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Student data file not found.");
+            }
+
+
+        }
+
+        public void DisplayStudents()
+        {
+            if (students.Count == 0)
+            {
+                Console.WriteLine("No student data available.");
+                return;
+            }
+
+            Console.WriteLine("Students (Sorted by Name):");
+            var sortedStudents = students.OrderBy(s => s.Name).ToList();
+
+            foreach (var student in sortedStudents)
+            {
+                Console.WriteLine($"Name: {student.Name}, Id: {student.Id}, Address: {student.Address}, Class: {student.Class}, Grade: {student.Grade}");
+            }
+        }
+
+        public void SearchStudentByName(string searchName)
+        {
+            var foundStudents = students
+                .Where(s => IsNameMatch(s.Name, searchName))
+                .ToList();
+
+            if (foundStudents.Count == 0)
+            {
+                Console.WriteLine("No students found with the given name.");
+            }
+            else
+            {
+                Console.WriteLine("Found Students:");
+                foreach (var student in foundStudents)
+                {
+                    Console.WriteLine($"Name: {student.Name}, Id: {student.Id}, Address: {student.Address}, Class: {student.Class}, Grade: {student.Grade}");
+                }
+            }
+        }
+
+        private bool IsNameMatch(string studentName, string searchName)
+        {
+            return studentName.Equals(searchName, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
